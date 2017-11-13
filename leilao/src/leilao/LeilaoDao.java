a992f4ecc3d4d78295ac7e7d3ee1fc9aafa5c215
Tales Wallace Souza <tales.wallace@gmail.com>
2017-11-13 20:04:32 -0200

    Este código comentado estava na classe errada, ele não é necessário aqui, mas sim na classe EncerradorDeLeilaoTest

--- leilao/src/leilao/LeilaoDao.java
+++ leilao/src/leilao/LeilaoDao.java
@@ -4,30 +4,30 @@ import java.util.Calendar;
 import java.util.List;
 
 public class LeilaoDao {
-    LeilaoDao daoFalso = mock(LeilaoDao.class);
-    
-    Calendar antiga = Calendar.getInstance();
-    antiga.set(1999, 1, 20);
-    Leilao leilao1 = new CriadorDeLeilao().para("TV de plasma").naData(antiga).constroi();
-    Leilao leilao2 = new CriadorDeLeilao().para("Geladeira").naData(antiga).constroi();
-    
-    List<Leilao> leiloesAntigos = Arrays.asList(leilao1, leilao2);
-    // criando o mock!
-    LeilaoDao daoFalso = mock(LeilaoDao.class);
-    // ensinando o mock a reagir da maneira que esperamos!
-    when(daoFalso.correntes()).thenReturn(leiloesAntigos);
-
-    private LeilaoDao mock(Class<LeilaoDao> aClass) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private static class CriadorDeLeilao {
-
-        public CriadorDeLeilao() {
-        }
-
-        private Object para(String tv_de_plasma) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-    }
+//    LeilaoDao daoFalso = mock(LeilaoDao.class);
+//    
+//    Calendar antiga = Calendar.getInstance();
+//    antiga.set(1999, 1, 20);
+//    Leilao leilao1 = new CriadorDeLeilao().para("TV de plasma").naData(antiga).constroi();
+//    Leilao leilao2 = new CriadorDeLeilao().para("Geladeira").naData(antiga).constroi();
+//    
+//    List<Leilao> leiloesAntigos = Arrays.asList(leilao1, leilao2);
+//    // criando o mock!
+//    LeilaoDao daoFalso = mock(LeilaoDao.class);
+//    // ensinando o mock a reagir da maneira que esperamos!
+//    when(daoFalso.correntes()).thenReturn(leiloesAntigos);
+//
+//    private LeilaoDao mock(Class<LeilaoDao> aClass) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+//
+//    private static class CriadorDeLeilao {
+//
+//        public CriadorDeLeilao() {
+//        }
+//
+//        private Object para(String tv_de_plasma) {
+//            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//        }
+//    }
 }
