f326858536351d9923bf524812c2012d59f73129
Tales Wallace Souza <tales.wallace@gmail.com>
2017-11-13 20:01:31 -0200

    Você não havia criado a classe RepositorioDeLeiloes, por isso ainda ficou dando erro, na declaração do dao e nas chamadas dos métodos dele.

---
+++ leilao/src/leilao/RepositorioDeLeiloes.java
@@ -0,0 +1,15 @@
+package leilao;
+
+import java.util.List;
+
+class RepositorioDeLeiloes {
+
+    void atualiza(Leilao leilao) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    List<Leilao> correntes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
